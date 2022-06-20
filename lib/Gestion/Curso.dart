a6e8466398cf7c3d5a1027878f735c388cda1e75
KotaDT <fabriziol.adam@gmail.com>
2022-06-20 14:41:39 -0400

    first commit

---
+++ lib/Gestion/Curso.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Curso extends StatefulWidget {
+  Curso({Key? key}) : super(key: key);
+
+  @override
+  State<Curso> createState() => _CursoState();
+}
+
+class _CursoState extends State<Curso> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/Gestion/Niños.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Kids extends StatefulWidget {
+  Kids({Key? key}) : super(key: key);
+
+  @override
+  State<Kids> createState() => _KidsState();
+}
+
+class _KidsState extends State<Kids> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
